b41e7ce7fb5ce55daae8070bbc57edd3b69700f2
Jai <malhi.jaivir@gmail.com>
2024-08-02 20:24:34 -0700

    Added experience section

--- src/lib/components/sections/Experience.svelte
+++ src/lib/components/sections/Experience.svelte
@@ -1,3 +1,93 @@
 <div id="experience" class="w-11/12 lg:w-5/6 flex flex-col justify-center items-center">
 	<h1 class="text-5xl font-bold underline underline-offset-4 mb-6 decoration-accent">Experience</h1>
+	<ul class="timeline timeline-snap-icon timeline-compact timeline-vertical">
+		<li>
+			<div class="timeline-middle">
+				<img
+					src="/logos/blueprint-logo.png"
+					alt="Blueprint Logo"
+					width="48"
+					height="48"
+					class="rounded-md"
+				/>
+			</div>
+			<div class="timeline-end mb-10 ml-4">
+				<time class="font-mono italic">SEPT 2023 - PRESENT</time>
+				<div class="text-lg font-black">Project Developer | SFU Blueprint</div>
+				<ul class="list-disc ml-6">
+					<li>Developed responsive web applications utilizing React and NextJS.</li>
+					<li>
+						Led front-end development efforts, ensuring high-quality user experiences and seamless
+						interactions.
+					</li>
+					<li>
+						Conducted thoroughcode reviews, enhancing code quality and performance, and collaborated
+						through Agile sprintsusing GitHub
+					</li>
+				</ul>
+			</div>
+			<hr class="bg-primary" />
+		</li>
+		<li>
+			<hr class="bg-primary" />
+			<div class="timeline-middle">
+				<img
+					src="/logos/zebrarobotics-logo.png"
+					alt="Zebra Robotics Logo"
+					width="48"
+					height="48"
+					class="rounded-md"
+				/>
+			</div>
+			<div class="timeline-end mb-10 ml-4">
+				<time class="font-mono italic">AUG 2023 - SEPT 2024</time>
+				<div class="text-lg font-black">Coding & Robotics Instructor | Zebra Robotics</div>
+				<ul class="list-disc ml-6">
+					<li>
+						Responsible for instructing children ages 6-17 about programming and providing guidance
+						as required
+					</li>
+					<li>
+						Teaching HTML/CSS, JavaScript, and Python ranging from beginner to advanced levels
+					</li>
+					<li>Training incoming instructors in company software & teaching etiquette</li>
+					<li>
+						Assisting with admin responsibilities including management & local school engagement
+					</li>
+				</ul>
+			</div>
+			<hr class="bg-primary" />
+		</li>
+		<li>
+			<hr class="bg-primary" />
+			<div class="timeline-middle">
+				<img
+					src="/logos/caminosoft-logo.png"
+					alt="CaminoSoft Logo"
+					width="48"
+					height="48"
+					class="rounded-md"
+				/>
+			</div>
+			<div class="timeline-end mb-10 ml-4">
+				<time class="font-mono italic">JAN 2022 - AUG 2022</time>
+				<div class="text-lg font-black">Software Developer Intern | CaminoSoft</div>
+				<ul class="list-disc ml-6">
+					<li>
+						Developed Data Science applications in Python using libraries like Pandas and NumPy
+					</li>
+					<li>
+						Improved data parser (csv inputs) runtime by ~20% by utilizing the Pandas library’s
+						chunking capabilities while maintaining low memory usage
+					</li>
+					<li>Assisted in maintaining code style and efficiency of programs by refactoring code</li>
+					<li>
+						Designed infrastructure automation process to deploy updates on client machines using
+						Ansible and YAML
+					</li>
+				</ul>
+			</div>
+			<hr class="bg-primary" />
+		</li>
+	</ul>
 </div>
