ecd05a2de6483b52328eabb52a5ae9cc90a1e0f1
Jai <malhi.jaivir@gmail.com>
2024-08-03 14:32:46 -0700

    Created projects section

---
+++ src/lib/utils/ProjectBlock.svelte
@@ -0,0 +1,21 @@
+<!-- IMAGES MUST BE 500px x 500px -->
+<script lang="ts">
+	export let src = '/projects/no-image.png';
+	export let alt = 'Image Not Available';
+	export let href = '/';
+
+	$: {
+		if (src === '' || src === undefined || src === null) {
+			src = '/projects/no-image.png';
+		}
+	}
+</script>
+
+<a {href} class="text-center hover:text-accent">
+	<img
+		{src}
+		alt={alt + ' Project'}
+		class="rounded-lg shadow-lg shadow-white/50 hover:shadow-accent/50 mb-2"
+	/>
+	<p class="font-semibold text-lg">{alt}</p>
+</a>
