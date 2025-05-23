db7a921de43f9e83398e875100ff07942f8e30c8
Jai <malhi.jaivir@gmail.com>
2025-05-22 22:49:42 -0700

    Update resume link

--- src/lib/utils/links.ts
+++ src/lib/utils/links.ts
@@ -2,7 +2,7 @@ export const links = {
 	linkedin: 'https://www.linkedin.com/in/jaimalhi/',
 	github: 'https://github.com/jaimalhi',
 	email: 'jai_malhi@sfu.ca',
-	resume: 'https://drive.google.com/file/d/1y4x2FR_m6WtuBV0iiFXyrZvUR1he2BRo/view?usp=sharing',
+	resume: 'https://drive.google.com/file/d/1dOH4V2VdlhyOs2m1sas2hBjnLN24EKR4/view?usp=drive_link',
 	portfolio: 'https://www.jaimalhi.ca/'
 };
 
